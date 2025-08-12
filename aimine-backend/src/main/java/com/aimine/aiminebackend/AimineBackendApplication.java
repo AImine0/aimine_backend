4dbdce3e7f1c82732d8eeea0b4872537e52930bb
Lee Sumin <lsmlsm426@knu.ac.kr>
2025-08-12 22:26:21 +0900

    BE 프로젝트 생성

---
+++ aimine-backend/src/main/java/com/aimine/aiminebackend/AimineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aimine.aiminebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimineBackendApplication.class, args);
+    }
+
+}
---
+++ aimine-backend/src/test/java/com/aimine/aiminebackend/AimineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimine.aiminebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
